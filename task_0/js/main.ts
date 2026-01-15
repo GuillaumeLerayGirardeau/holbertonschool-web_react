505954a4c124f47b03525b1140d6db3ff035d136
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2026-01-15 14:52:48 +0100

    add: task_0 added

---
+++ task_0/js/main.ts
@@ -0,0 +1,44 @@
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string
+}
+
+let student1: Student = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 25,
+    location: 'New-York'
+}
+
+let student2: Student = {
+    firstName: 'Lou',
+    lastName: 'Smith',
+    age: 28,
+    location: 'Boston',
+}
+
+let studentsList: Student[] = [student1, student2];
+
+const table = document.createElement("table");
+const tbody = document.createElement("tbody");
+
+for(const student of studentsList) {
+    const row = document.createElement("tr");
+
+    const firstNameCell = document.createElement("td");
+    firstNameCell.textContent = student.firstName;
+
+    const locationCell = document.createElement("td");
+    locationCell.textContent = student.location;
+
+    row.appendChild(firstNameCell);
+    row.appendChild(locationCell);
+
+    tbody.appendChild(row);
+}
+
+table.appendChild(tbody);
+
+document.body.appendChild(table);
