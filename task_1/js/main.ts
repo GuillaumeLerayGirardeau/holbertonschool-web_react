fe3148bff067a0c4c59247ff5c76b5d4e30b7cb0
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2026-01-15 16:21:23 +0100

    add: Directors interface created

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -8,20 +8,6 @@ interface Teacher {
     [key: string]: any;
 }
 
-const teacher3: Teacher = {
-  firstName: 'John',
-  fullTimeEmployee: false,
-  lastName: 'Doe',
-  location: 'London',
-  contract: false,
-};
-
-console.log(teacher3);
-
-// should print
-// Object
-// contract: false
-// firstName: "John"
-// fullTimeEmployee: false
-// lastName: "Doe"
-// location: "London"
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
