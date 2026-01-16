50d19ec248ff9dba96eb59db461ccaa888f6b5b0
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2026-01-16 10:40:27 +0100

    add: task 5 added

---
+++ task_2/js/main.ts
@@ -0,0 +1,49 @@
+// Interfaces for Director and teacher classes
+interface DirectorInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;  
+}
+
+// Classes Director and Teacher, implementing their interfaces
+class Director implements DirectorInterface {
+    workFromHome(): string {
+        return ('Working from home');
+    }
+
+    getCoffeeBreak(): string {
+        return ('Getting a coffee break');
+    }
+
+    workDirectorTasks(): string {
+        return ('Getting to director tasks');
+    }
+}
+
+class Teacher implements TeacherInterface {
+    workFromHome(): string {
+        return ('Cannot work from home');
+    }
+
+    getCoffeeBreak(): string {
+        return ('Cannot have a break');
+    }
+
+    workTeacherTasks(): string {
+        return ('Getting to work');
+    }
+}
+
+// Function creating an instace of a Teacher or Director, based on the salary variable
+function createEmployee(salary: (number | string)): (TeacherInterface | DirectorInterface) {
+    if (typeof salary === 'number' && salary < 500) {
+        return new Teacher();
+    }
+    return new Director();
+}
