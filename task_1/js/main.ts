47b13a1209a11bbcdfd6aad7528b15e8ee0d4a6d
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2026-01-16 10:11:22 +0100

    add: StudentClass class added, with class and constructor interfaces

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -1,3 +1,4 @@
+// Interface Teacher 
 interface Teacher {
     readonly firstName: string;
     readonly lastName: string;
@@ -8,14 +9,44 @@ interface Teacher {
     [key: string]: any;
 }
 
+// Extension de l'interface teacher avec l'interface Directors
 interface Directors extends Teacher {
     numberOfReports: number;
 }
 
+//Interface définissant les arguments de la fonction printTeacher
 interface printTeacherFunction {
     (firstName: string, lastName: string): string;
 }
 
+// Fontion printTeacher
 const printTeacher: printTeacherFunction = (firstName, lastName) => {
     return (`${firstName[0]}. ${lastName}`);
+}
+
+
+// Interface de la classe StudentClass
+interface StudentClassInterface {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+//Interface du constructeur de la classe StudentClass
+interface StudentConstructorInterface {
+    new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+// Classe StudentClass, implémentant une interface pour la classe et le constructeur
+class StudentClass implements StudentClassInterface {
+    constructor(public firstName: string, public lastName: string) {}
+
+    workOnHomework(): string {
+        return ('Currently working');
+    }
+
+    displayName(): string {
+        return (this.firstName);
+    }
 }
